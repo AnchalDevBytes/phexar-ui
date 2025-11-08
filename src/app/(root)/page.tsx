5d6a0c8202db042d477330e15e990f39b179e982
anchalraj31082004 <anchalraj20045@gmail.com>
2025-11-08 12:01:38 +0530

    typo fixes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,29 +5,32 @@ import AIInput_04 from "@/components/phexarui/ai-input/ai-input-04";
 import AIInput_05 from "@/components/phexarui/ai-input/ai-input-05";
 import AIInput_06 from "@/components/phexarui/ai-input/ai-input-06";
 import HeroSection from "@/components/Landing/Hero";
+import Link from "next/link";
 // import { div } from "motion/react-client";
 // import Link from "next/link";
 
 
 const Home = () => {
   return (
-    // <div className="h-screen w-full flex flex-col items-center py-20 gap-10 bg-slate-200 dark:bg-slate-900">
-    //   <h1 className="text-3xl text-transparent bg-clip-text bg-linear-to-r from bg-slate-300 via-slate-700 to-slate-800 tracking-wider font-medium">Home</h1>
-    //   <Link href="/playground" className="px-10 py-2 bg-slate-300 hover:bg-slate-400 hover:ring-2 outline-offset-2 border border-slate-200 rounded-md">Playground</Link>
-    // </div>
-
-    <div className="bg-white dark:bg-black/5 overflow-x-hidden">
-      <div className="grid grid-rows-[auto_1fr_auto] min-h-screen px-6 lg:px-4 gap-4 sm:gap-12">
-        <HeroSection/>
-
-        {/* <AiInput01/>
-        <AIInput_02/>
-        <AIInput_03/>
-        <AIInput_04/>
-        <AIInput_05/>
-        <AIInput_06/> */}
-      </div>
+    <div className="h-screen w-full flex flex-col items-center py-20 gap-10 bg-slate-200 dark:bg-slate-900">
+      <h1 className="text-3xl text-transparent bg-clip-text bg-linear-to-r from bg-slate-300 via-slate-700 to-slate-800 tracking-wider font-medium">Home</h1>
+      <Link href="/playground" className="px-10 py-2 bg-slate-300 hover:bg-slate-400 hover:ring-2 outline-offset-2 border border-slate-200 rounded-md">Playground</Link>
     </div>
+
+    // <div className="bg-white dark:bg-black/5 overflow-x-hidden">
+    //   <div className="grid grid-rows-[auto_1fr_auto] min-h-screen px-6 lg:px-4 gap-4 sm:gap-12">
+    //     <HeroSection/>
+
+    //     {/* <AiInput01/>
+    //     <AIInput_02/>
+    //     <AIInput_03/>
+    //     <AIInput_04/>
+    //     <AIInput_05/>
+    //     <AIInput_06/> */}
+
+
+    //   </div>
+    // </div>
   )
 }
 
