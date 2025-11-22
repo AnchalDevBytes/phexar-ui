afc7173d040425fcbd34b452071f2bbebdaa6d8b
anchalraj31082004 <anchalraj20045@gmail.com>
2025-11-22 13:04:04 +0530

    changed fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-providers";
 import { RootProvider } from "fumadocs-ui/provider/next";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen` }
+        className={`${montserrat.variable} ${roboto.variable} antialiased flex flex-col min-h-screen` }
       >
         <RootProvider>
           <ThemeProvider
